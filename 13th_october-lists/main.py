ef433ce9fca3ede1b5196ae525ee957bf3c1078c
Ibrahim-07-ibu <ibbux.07@gmail.com>
2025-10-13 13:29:09 +0530

    added new file

---
+++ 13th_october-lists/main.py
@@ -0,0 +1,15 @@
+# case1 
+#hard coding
+even_numbers = [2,4,6,8,10]
+print(even_numbers)
+ 
+print(even_numbers[2])
+
+print(even_numbers[4])
+
+#case 2
+#for all time of cases
+length = len(even_numbers)
+print("length of the even numberlist is ",length)
+
+print(even_numbers[length-1])
